7dd46d8ccd3c262ab397ed2975a39e039d8c1b2d
Josuah Demangeon <me@josuah.net>
2022-08-28 14:21:08 +0200

    able to write content to a flash chip
    Taking inspiration on the pico-examples repository for how to handle
    this with the pico-sdk, the following code was tested to work with an
    MX25L3233F flash chip:
    
    void
    print_page(uint8_t const *page)
    {
    	for (size_t i = 0; i < FLASH_PAGE_SIZE; i++) {
    		printf(" %02X", page[i]);
    		if ((i + 1) % 32 == 0)
    			printf("\n");
    	}
    }
    
    int
    main(void)
    {
    	uint8_t page[FLASH_PAGE_SIZE] = {0};
    
    	stdio_init_all();
    
    	gpio_init(25);
    	gpio_set_dir_out_masked(1 << 25);
    	gpio_put(25, true);
    
    	spi_init(spi_default, 1000 * 1000);
    	gpio_set_function(PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
    	gpio_set_function(PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
    	gpio_set_function(PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
    	gpio_init(PICO_DEFAULT_SPI_CSN_PIN);
    	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, 1);
    	gpio_set_dir(PICO_DEFAULT_SPI_CSN_PIN, GPIO_OUT);
    
    	memset(page, 0xFF, sizeof page);
    
    	flash_program_page(spi_default, 0x000000, page);
    	for (;;) {
    		flash_read_page(spi_default, 0x000000, page);
    		print_page(page);
    	}
    
    	return 0;
    }

--- include/pico-ice/flash.h
+++ include/pico-ice/flash.h
@@ -2,5 +2,8 @@
 #include "hardware/spi.h"
 
 #define FLASH_PAGE_SIZE		256
+#define spi_flash		spi1
 
-void flash_read(spi_hw_t *spi, uint32_t addr, uint8_t *buf, size_t sz);
+void flash_read(spi_inst_t *spi, uint32_t addr, uint8_t *buf, size_t sz);
+void flash_read_page(spi_inst_t *spi, uint32_t addr, uint8_t page[FLASH_PAGE_SIZE]);
+void flash_program_page(spi_inst_t *spi, uint32_t addr, uint8_t const page[FLASH_PAGE_SIZE]);
--- src/flash.c
+++ src/flash.c
@@ -1,16 +1,87 @@
 #include "pico/stdlib.h"
-#include "pico/binary_info.h"
 #include "hardware/spi.h"
+#include "pico-ice/flash.h"
 
-#define FLASH_CMD_READ	0x03
+#define FLASH_CMD_PROGRAM_PAGE	0x02
+#define FLASH_CMD_READ		0x03
+#define FLASH_CMD_ENABLE_WRITE	0x06
+#define FLASH_CMD_STATUS	0x05
+
+#define FLASH_STATUS_BUSY_MASK	0x01
+
+static void
+cs_select(void)
+{
+	sleep_us(1);
+	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, false);
+	sleep_us(1);
+}
+
+static void
+cs_deselect(void)
+{
+	sleep_us(1);
+	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, true);
+	sleep_us(1);
+}
+
+static void
+flash_wait(spi_inst_t *spi)
+{
+	uint8_t cmds[] = { FLASH_CMD_STATUS, 0 };
+	uint8_t buf[2];
+
+	do {
+		cs_select();
+		spi_write_read_blocking(spi, cmds, buf, 2);
+		cs_deselect();
+	} while (buf[0] & FLASH_STATUS_BUSY_MASK);
+}
+
+static void
+flash_enable_write(spi_inst_t *spi)
+{
+	uint8_t cmds[] = { FLASH_CMD_ENABLE_WRITE };
+
+	cs_select();
+	spi_write_blocking(spi, cmds, sizeof cmds);
+	cs_deselect();
+}
+
+void
+flash_program_page(spi_inst_t *spi, uint32_t addr, uint8_t const page[FLASH_PAGE_SIZE])
+{
+	uint8_t cmds[] = { FLASH_CMD_PROGRAM_PAGE, addr >> 16, addr >> 8, addr };
+
+	assert(addr % FLASH_PAGE_SIZE == 0);
+
+	flash_enable_write(spi);
+	cs_select();
+	spi_write_blocking(spi, cmds, sizeof cmds);
+	spi_write_blocking(spi, page, FLASH_PAGE_SIZE);
+	cs_deselect();
+	flash_wait(spi);
+}
+
+void
+flash_read_page(spi_inst_t *spi, uint32_t addr, uint8_t page[FLASH_PAGE_SIZE])
+{
+	flash_read(spi, addr, page, FLASH_PAGE_SIZE);
+}
 
 void
 flash_read(spi_inst_t *spi, uint32_t addr, uint8_t *buf, size_t sz)
 {
 	uint8_t cmds[] = { FLASH_CMD_READ, addr >> 16, addr >> 8, addr };
 
+	cs_select();
 	spi_write_blocking(spi, cmds, sizeof cmds);
-	sleep_us(1);
 	spi_read_blocking(spi, 0x00, buf, sz);
-	sleep_us(1);
+	cs_deselect();
+}
+
+void
+flash_sector_erase(spi_inst_t *spi_default, uint8_t addr)
+{
+
 }
