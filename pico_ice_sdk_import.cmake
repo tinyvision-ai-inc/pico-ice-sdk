865c1f3e90ebeaaf0170c707009053c5c204b5ff
Josuah Demangeon <me@josuah.net>
2022-09-05 14:52:41 +0200

    flatten and simplify the CMakeLists.txt

--- pico_ice_sdk_import.cmake
+++ pico_ice_sdk_import.cmake
@@ -1,6 +1,7 @@
-# This is a copy of {PICO_ICE_SDK_PATH}/cmake/pico_ice_sdk_import.cmake
+# This is a copy of {PICO_ICE_SDK_PATH}/pico_ice_sdk_import.cmake
 include(FetchContent)
 
+message("Downloading Raspberry Pi Pico SDK")
 FetchContent_Declare(
     pico-sdk
     GIT_REPOSITORY https://github.com/raspberrypi/pico-sdk
@@ -8,7 +9,10 @@ FetchContent_Declare(
     GIT_SHALLOW true
     GIT_SUBMODULES_RECURSE false
 )
+FetchContent_Populate(pico-sdk)
+set(PICO_SDK_PATH ${pico-sdk_SOURCE_DIR} CACHE PATH "Path to the Raspberry Pi Pico SDK")
 
+message("Downloading Pico-Ice SDK")
 FetchContent_Declare(
     pico-ice-sdk
     GIT_REPOSITORY https://github.com/tinyvision-ai-inc/pico-ice-sdk
@@ -16,21 +20,8 @@ FetchContent_Declare(
     GIT_SHALLOW true
     GIT_SUBMODULES_RECURSE false
 )
-
-if(NOT pico-sdk)
-    message("Downloading Raspberry Pi Pico SDK")
-    FetchContent_Populate(pico-sdk)
-    set(PICO_SDK_PATH ${pico-sdk_SOURCE_DIR})
-endif()
-
-if(NOT pico-ice-sdk)
-    message("Downloading Pico-Ice SDK")
-    FetchContent_Populate(pico-ice-sdk)
-    set(PICO_ICE_SDK_PATH ${pico-ice-sdk_SOURCE_DIR})
-endif()
-
-set(PICO_SDK_PATH ${PICO_SDK_PATH} CACHE PATH "Path to the Raspberry Pi Pico SDK")
-set(PICO_ICE_SDK_PATH ${PICO_ICE_SDK_PATH} CACHE PATH "Path to the Pico-Ice SDK")
+FetchContent_Populate(pico-ice-sdk)
+set(PICO_ICE_SDK_PATH ${pico-ice-sdk_SOURCE_DIR} CACHE PATH "Path to the Pico-Ice SDK")
 
 include(${PICO_SDK_PATH}/pico_sdk_init.cmake)
 add_subdirectory(${PICO_ICE_SDK_PATH})
