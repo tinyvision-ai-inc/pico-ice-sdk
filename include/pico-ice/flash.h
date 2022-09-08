d3c033426891002161e412bacfc097f5e2bca254
Josuah Demangeon <me@josuah.net>
2022-09-08 19:19:39 +0200

    flash: add documentation in the header

--- include/pico-ice/flash.h
+++ include/pico-ice/flash.h
@@ -1,9 +1,55 @@
 #pragma once
 #include "hardware/spi.h"
 
-#define FLASH_PAGE_SIZE		256
-#define spi_flash		spi1
+/*
+ * API for communicating with flash chips: reading and writing.
+ */
 
-void flash_read(spi_inst_t *spi, uint32_t addr, uint8_t *buf, size_t sz);
-void flash_read_page(spi_inst_t *spi, uint32_t addr, uint8_t page[FLASH_PAGE_SIZE]);
-void flash_program_page(spi_inst_t *spi, uint32_t addr, uint8_t const page[FLASH_PAGE_SIZE]);
+#define FLASH_PAGE_SIZE		256
+
+#define spi_fpga_flash		spi1
+/*
+ * The flash peripheral instance that is connected to the FGPA's flash chip.
+ */
+
+#define FLASH_FPGA_SPI_SCK_PIN	2
+#define FLASH_FPGA_SPI_TX_PIN	3
+#define FLASH_FPGA_SPI_RX_PIN	0
+#define FLASH_FPGA_SPI_CSN_PIN	1
+/*
+ * Pinout between the RP2040 and the FPGA's flash chip.
+ * These pins must be set at high-impedance/floating whenever not in use to program the flash chip,
+ * to not distrub the FPGA operation, in particular when the FPGA is under initialisation.
+ * This is handled by ``ice_flash_program()``.
+ *
+ * Each pin must be configured as SPI, except the CSN pin, to be set as a GPIO pin.
+ */
+
+void flash_read(spi_inst_t *spi, uint8_t pin, uint32_t addr, uint8_t *buf, size_t sz);
+/*
+ * Communicate to the chip over SPI and read multiple bytes at chosen address onto onto a buffer.
+ *
+ * :param spi: The SPI interface of the RP2040 to use.
+ * :param pin: The CS GPIO pin of the RP2040 to use.
+ * :param addr: The address at which the data is read.
+ * :param buf: The buffer onto which the data read is stored.
+ * :param sz: The size of ``buf``.
+ */
+
+void flash_program_page(spi_inst_t *spi, uint8_t pin, uint32_t addr, uint8_t const page[FLASH_PAGE_SIZE]);
+/*
+ * Program a page of the flash chip at the given address.
+ *
+ * :param spi: The SPI interface of the RP2040 to use.
+ * :param pin: The CS GPIO pin of the RP2040 to use.
+ * :param addr: The address at which the data is written.
+ * :param page: The buffer holding the data to be sent to the flash chip, of size ``FLASH_PAGE_SIZE``.
+ */
+
+void flash_erase_chip(spi_inst_t *spi);
+/*
+ * Send a command to erase the whole chip.
+ *
+ * :param spi: The SPI interface of the RP2040 to use.
+ * :param pin: The CS GPIO pin of the RP2040 to use.
+ */
--- src/flash.c
+++ src/flash.c
@@ -8,82 +8,82 @@
 #define FLASH_CMD_READ		0x03
 #define FLASH_CMD_ENABLE_WRITE	0x06
 #define FLASH_CMD_STATUS	0x05
+#define FLASH_CMD_CHIP_ERASE	0xC7
 
 #define FLASH_STATUS_BUSY_MASK	0x01
 
 static void
-cs_select(void)
+flash_chip_select(uint8_t pin)
 {
 	sleep_us(1);
-	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, false);
+	gpio_put(pin, false);
 	sleep_us(1);
 }
 
 static void
-cs_deselect(void)
+flash_chip_deselect(uint8_t pin)
 {
 	sleep_us(1);
-	gpio_put(PICO_DEFAULT_SPI_CSN_PIN, true);
+	gpio_put(pin, true);
 	sleep_us(1);
 }
 
 static void
-flash_wait(spi_inst_t *spi)
+flash_wait(spi_inst_t *spi, uint8_t pin)
 {
 	uint8_t cmds[] = { FLASH_CMD_STATUS, 0 };
 	uint8_t buf[2];
 
 	do {
-		cs_select();
+		flash_chip_select(pin);
 		spi_write_read_blocking(spi, cmds, buf, 2);
-		cs_deselect();
+		flash_chip_deselect(pin);
 	} while (buf[0] & FLASH_STATUS_BUSY_MASK);
 }
 
 static void
-flash_enable_write(spi_inst_t *spi)
+flash_enable_write(spi_inst_t *spi, uint8_t pin)
 {
 	uint8_t cmds[] = { FLASH_CMD_ENABLE_WRITE };
 
-	cs_select();
+	flash_chip_select(pin);
 	spi_write_blocking(spi, cmds, sizeof cmds);
-	cs_deselect();
+	flash_chip_deselect(pin);
 }
 
 void
-flash_program_page(spi_inst_t *spi, uint32_t addr, uint8_t const page[FLASH_PAGE_SIZE])
+flash_program_page(spi_inst_t *spi, uint8_t pin, uint32_t addr, uint8_t const page[FLASH_PAGE_SIZE])
 {
 	uint8_t cmds[] = { FLASH_CMD_PROGRAM_PAGE, addr >> 16, addr >> 8, addr };
 
 	assert(addr % FLASH_PAGE_SIZE == 0);
 
-	flash_enable_write(spi);
-	cs_select();
+	flash_enable_write(spi, pin);
+	flash_chip_select(pin);
 	spi_write_blocking(spi, cmds, sizeof cmds);
 	spi_write_blocking(spi, page, FLASH_PAGE_SIZE);
-	cs_deselect();
-	flash_wait(spi);
+	flash_chip_deselect(pin);
+	flash_wait(spi, pin);
 }
 
 void
-flash_read_page(spi_inst_t *spi, uint32_t addr, uint8_t page[FLASH_PAGE_SIZE])
-{
-	flash_read(spi, addr, page, FLASH_PAGE_SIZE);
-}
-
-void
-flash_read(spi_inst_t *spi, uint32_t addr, uint8_t *buf, size_t sz)
+flash_read(spi_inst_t *spi, uint8_t pin, uint32_t addr, uint8_t *buf, size_t sz)
 {
 	uint8_t cmds[] = { FLASH_CMD_READ, addr >> 16, addr >> 8, addr };
 
-	cs_select();
+	flash_chip_select(pin);
 	spi_write_blocking(spi, cmds, sizeof cmds);
 	spi_read_blocking(spi, 0x00, buf, sz);
-	cs_deselect();
+	flash_chip_deselect(pin);
 }
 
 void
-flash_sector_erase(spi_inst_t *spi, uint8_t addr)
+flash_chip_erase(spi_inst_t *spi, uint8_t pin)
 {
+	uint8_t cmds[] = { FLASH_CMD_CHIP_ERASE };
 
+	flash_chip_select(pin);
+	spi_write_blocking(spi, cmds, sizeof cmds);
+	flash_chip_deselect(pin);
+	flash_wait(spi, pin);
 }
