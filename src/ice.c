bec18b3678a6871934f8a8a52bc12a5951725a2d
Josuah Demangeon <me@josuah.net>
2022-09-13 13:06:05 +0200

    move custom pin definitions to boards/pico_ice.h

--- src/ice.c
+++ src/ice.c
@@ -14,14 +14,14 @@ ice_init_flash(void)
     spi_init(spi_fpga_flash, 1000 * 1000);
 
     // Setup the associated GPIO pins except CSN
-    gpio_set_function(ICE_FPGA_FLASH_SPI_SCK_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(ICE_FPGA_FLASH_SPI_TX_PIN, GPIO_FUNC_SPI);
-    gpio_set_function(ICE_FPGA_FLASH_SPI_RX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(ICE_FLASH_SPI_SCK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(ICE_FLASH_SPI_TX_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(ICE_FLASH_SPI_RX_PIN, GPIO_FUNC_SPI);
 
     // Setup the CSN pin to GPIO mode for manual control
-    gpio_init(ICE_FPGA_FLASH_SPI_CSN_PIN);
-    gpio_set_dir(ICE_FPGA_FLASH_SPI_CSN_PIN, GPIO_OUT);
-    gpio_put(ICE_FPGA_FLASH_SPI_CSN_PIN, 1);
+    gpio_init(ICE_FLASH_SPI_CSN_PIN);
+    gpio_set_dir(ICE_FLASH_SPI_CSN_PIN, GPIO_OUT);
+    gpio_put(ICE_FLASH_SPI_CSN_PIN, 1);
 
     // For assert()s below
     ice_flash_init_done = 1;
@@ -30,5 +30,5 @@ ice_init_flash(void)
 void
 ice_init_fpga_clock(uint8_t mhz)
 {
-	clock_gpio_init(PICO_ICE_FPGA_CLOCK_PIN, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_USB, 48 / mhz);
+	clock_gpio_init(ICE_FPGA_CLOCK_PIN, CLOCKS_CLK_GPOUT0_CTRL_AUXSRC_VALUE_CLK_USB, 48 / mhz);
 }
