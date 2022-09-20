8f4bdc2447f39bf4d555d01b7eb4cbf1cad3d44c
Josuah Demangeon <me@josuah.net>
2022-09-20 20:15:18 +0200

    adjust the comment style to Doxygen

--- include/pico_ice/ice.h
+++ include/pico_ice/ice.h
@@ -4,36 +4,36 @@
 #include "tusb.h"
 #include "bsp/board.h"
 
-/*
- * High-level API for driving the board.
+/**
+ * @brief High-level API for driving the board.
  * It is under heavy development and subject to change at any time!
  */
 
-void ice_init_defaults(void);
-/*
- * Call all functions below with default values:
+/**
+ * @brief Call all functions below with default values:
  * no need to call any other initialization function when this is called.
  */
+void ice_init_defaults(void);
 
-void ice_init_flash(void);
-/*
- * Initialise the SPI1 peripheral, dedicated to flashing the FPGA.
+/**
+ * @brief Initialise the SPI1 peripheral, dedicated to flashing the FPGA.
  */
+void ice_init_flash(void);
 
-void ice_init_usb(void);
-/*
- * Initialise the TinyUSB library, enabling the UART (CDC) and
+/**
+ * @brief Initialise the TinyUSB library, enabling the UART (CDC) and
  * drag-and-drop (MSC) interfaces.
  */
+void ice_init_usb(void);
 
-void ice_init_fpga(void);
-/*
- * Initialise the FPGA chip and communication with it.
- * This will start the FPGA.
+/**
+ * @brief Initialise the FPGA chip and communication with it.
+ * This will start the FPGA clock and take it out of reset.
  */
+void ice_init_fpga(void);
 
-void ice_usb_task(void);
-/*
- * Run all code related to USB in a non-blocking way.
+/**
+ * @brief Run all code related to USB in a non-blocking way.
  * It is typically to be placed at the end of the main application loop.
  */
+void ice_usb_task(void);
--- include/pico_ice/priv.h
+++ include/pico_ice/priv.h
@@ -2,8 +2,8 @@
  * All functions defined for internal use, not useful for using the SDK.
  */
 
-// src/usb_cdc.c
+/* src/usb_cdc.c */
 void tud_task_cdc(void);
 
-// src/tinyuf2/uf2.h
+/* src/tinyuf2/uf2.h */
 void uf2_init(void);
