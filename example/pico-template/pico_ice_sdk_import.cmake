9e1561bbca810e9e06ec59a7ca8e42e7e7b50f95
Josuah Demangeon <me@josuah.net>
2022-11-17 12:52:39 -0500

    improve compatibility with earlier CMake versions

--- example/pico-template/pico_ice_sdk_import.cmake
+++ example/pico-template/pico_ice_sdk_import.cmake
@@ -1,1 +1,43 @@
-../../cmake/pico_ice_sdk_import.cmake
+# This is a copy of {PICO_ICE_SDK_PATH}/pico_ice_sdk_import.cmake
+include(FetchContent)
+
+if (DEFINED ENV{PICO_SDK_PATH})
+    set(PICO_SDK_PATH "$ENV{PICO_SDK_PATH}")
+endif()
+
+if (DEFINED ENV{PICO_ICE_SDK_PATH})
+    set(PICO_ICE_SDK_PATH $ENV{PICO_ICE_SDK_PATH})
+endif()
+
+if (NOT DEFINED PICO_SDK_PATH)
+    message("Downloading Raspberry Pi Pico SDK")
+    FetchContent_Declare(
+        pico-sdk
+        GIT_REPOSITORY https://github.com/raspberrypi/pico-sdk
+        GIT_TAG master
+        GIT_SHALLOW true
+        GIT_SUBMODULES_RECURSE false
+    )
+    FetchContent_Populate(pico-sdk)
+    set(PICO_SDK_PATH ${pico-sdk_SOURCE_DIR} CACHE PATH "Path to the Raspberry Pi Pico SDK")
+endif()
+
+if (NOT DEFINED PICO_ICE_SDK_PATH)
+    message("Downloading Pico-Ice SDK")
+    FetchContent_Declare(
+        pico-ice-sdk
+        GIT_REPOSITORY https://github.com/tinyvision-ai-inc/pico-ice-sdk
+        GIT_TAG main
+        GIT_SHALLOW true
+        GIT_SUBMODULES_RECURSE false
+    )
+    FetchContent_Populate(pico-ice-sdk)
+    set(PICO_ICE_SDK_PATH ${pico-ice-sdk_SOURCE_DIR} CACHE PATH "Path to the Pico-Ice SDK")
+endif()
+
+message("PICO_SDK_PATH=$PICO_SDK_PATH")
+message("PICO_ICE_SDK_PATH=$PICO_ICE_SDK_PATH")
+
+include(${PICO_ICE_SDK_PATH}/cmake/pico_ice_sdk_init.cmake)
+include(${PICO_SDK_PATH}/pico_sdk_init.cmake)
+add_subdirectory(${PICO_ICE_SDK_PATH})
