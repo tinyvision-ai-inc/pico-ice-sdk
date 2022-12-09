3c7b659a5ed03a5c107edf00673c25f411c68f92
Al <alastairpatrick@gmail.com>
2022-12-08 21:45:24 -0800

    Serial memory async completion callback

--- example/pico-ssram/firmware.c
+++ example/pico-ssram/firmware.c
@@ -12,10 +12,17 @@
 #define DATA_LEN 8
 #define START_ADDR 0
 
+static volatile int g_sequence_count;
+
+void async_callback(void) {
+    ++g_sequence_count;
+}
+
 int main() {
     ice_sdk_init();
     stdio_init_all();
     ice_ssram_init(10*1000*1000, DMA_IRQ_1 /* Pass -1 for synchronous mode */);
+    ice_ssram_set_async_callback(async_callback);
 
     // Dont let the FPGA on the bus so we get exclusive access
     ice_fpga_halt();
@@ -28,9 +35,9 @@ int main() {
 
     for (;;) {
         ice_usb_task();
-        ice_ssram_write(ICE_SSRAM_SPI_CS_PIN, START_ADDR, write_data, sizeof(write_data));
-        ice_ssram_read(ICE_SSRAM_SPI_CS_PIN, read_data, START_ADDR, sizeof(read_data));
-        ice_ssram_await_async_complete();
+        ice_ssram_write(ICE_SSRAM_SPI_CS_PIN, START_ADDR, write_data, sizeof(write_data), true /* async */);
+        ice_ssram_read(ICE_SSRAM_SPI_CS_PIN, read_data, START_ADDR, sizeof(read_data), true /* async */);
+        ice_ssram_await_async_completion();
 
         for (size_t i = 0; i < DATA_LEN; i++) {
             if (read_data[i] != i) {
--- include/ice/ssram.h
+++ include/ice/ssram.h
@@ -12,19 +12,22 @@
 #include <stddef.h>
 #include "boards/pico_ice.h"
 
+typedef void (*ice_ssram_async_callback_t)(void);
+
 // Common interface
 void ice_ssram_init(int bit_rate, int irq /* -1 for synchronous mode */);
 void ice_ssram_deinit(void);
 bool ice_ssram_is_async_complete(void);
-void ice_ssram_await_async_complete(void);
+void ice_ssram_await_async_completion(void);
+void ice_ssram_set_async_callback(ice_ssram_async_callback_t callback);
 
 // Higher level interface
 uint8_t ice_ssram_get_status(int cs_pin);
 void ice_ssram_erase_chip(int cs_pin);
 void ice_ssram_erase_sector(int cs_pin, uint32_t dest_addr);
 void ice_ssram_enable_write(int cs_pin, bool enabled);
-void ice_ssram_write(int cs_pin, uint32_t dest_addr, const void* src, uint32_t size);
-void ice_ssram_read(int cs_pin, void* dest, uint32_t src_addr, uint32_t size);
+void ice_ssram_write(int cs_pin, uint32_t dest_addr, const void* src, uint32_t size, bool async);
+void ice_ssram_read(int cs_pin, void* dest, uint32_t src_addr, uint32_t size, bool async);
 void ice_ssram_enable_power(int cs_pin, bool enabled);
 
 // Lower level interface
