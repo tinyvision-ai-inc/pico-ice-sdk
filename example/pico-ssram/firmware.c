8dab7968e83c40de40933efbbdfae0b0653e17e3
Al <alastairpatrick@gmail.com>
2022-12-10 11:11:32 -0800

    Rename "ssram" -> "smem"

--- example/pico-ssram/firmware.c
+++ example/pico-ssram/firmware.c
@@ -7,7 +7,7 @@
 #include "hardware/spi.h"
 #include "ice/sdk.h"
 #include "ice/fpga.h"
-#include "ice/ssram.h"
+#include "ice/smem.h"
 
 #define DATA_LEN 8
 #define START_ADDR 0
@@ -21,8 +21,8 @@ void async_callback(void) {
 int main() {
     ice_sdk_init();
     stdio_init_all();
-    ice_ssram_init(10*1000*1000, DMA_IRQ_1 /* Pass -1 for synchronous mode */);
-    ice_ssram_set_async_callback(async_callback);
+    ice_smem_init(10*1000*1000, DMA_IRQ_1 /* Pass -1 for synchronous mode */);
+    ice_smem_set_async_callback(async_callback);
 
     // Dont let the FPGA on the bus so we get exclusive access
     ice_fpga_halt();
@@ -35,9 +35,9 @@ int main() {
 
     for (;;) {
         ice_usb_task();
-        ice_ssram_write_async(ICE_SSRAM_SPI_CS_PIN, START_ADDR, write_data, sizeof(write_data));
-        ice_ssram_read_async(ICE_SSRAM_SPI_CS_PIN, read_data, START_ADDR, sizeof(read_data));
-        ice_ssram_await_async_completion();
+        ice_smem_write_async(ICE_SSRAM_SPI_CS_PIN, START_ADDR, write_data, sizeof(write_data));
+        ice_smem_read_async(ICE_SSRAM_SPI_CS_PIN, read_data, START_ADDR, sizeof(read_data));
+        ice_smem_await_async_completion();
 
         for (size_t i = 0; i < DATA_LEN; i++) {
             if (read_data[i] != i) {
