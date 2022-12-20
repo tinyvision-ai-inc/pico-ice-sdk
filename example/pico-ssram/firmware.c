200568959e4a9b104503c485e8871e4414786be1
Al <alastairpatrick@gmail.com>
2022-12-20 11:27:28 -0800

    Split up pico_ice_sdk libraries

--- example/pico-ssram/firmware.c
+++ example/pico-ssram/firmware.c
@@ -3,14 +3,11 @@
 
 #include "boards/pico_ice.h"
 
-#include "hardware/dma.h"
 #include "hardware/irq.h"
-#include "hardware/spi.h"
 
-#include "ice_usb.h"
-#include "ice_sdk.h"
-#include "ice_fpga.h"
 #include "ice_smem.h"
+
+#include <stdio.h>
 
 #define DATA_LEN 8
 #define START_ADDR 0
@@ -23,20 +20,14 @@ void write_callback(void* context) {
 }
 
 int main() {
-    ice_sdk_init();
     stdio_init_all();
     ice_smem_init(10*1000*1000, DMA_IRQ_1 /* Pass -1 for synchronous mode */);
-
-    // Dont let the FPGA on the bus so we get exclusive access
-    ice_fpga_halt();
 
     for (uint16_t i = 0; i < DATA_LEN; i++) {
         write_data[i] = i;
     }
 
     for (;;) {
-        tud_task();
-
         // The write callback chains a read sequence immediately after the write sequence from an interrupt handler,
         // without any interaction with the main thread.
         ice_smem_write_async(ICE_SSRAM_SPI_CS_PIN, START_ADDR, write_data, sizeof(write_data), write_callback, NULL);
