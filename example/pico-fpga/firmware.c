95dd0d6bac90ac24a17dbbaf7a71438da54af6c5
Venkat Rangan <venkat@tinyvision.ai>
2023-01-10 14:16:21 -0800

    Updated FPGA example so it lets go of the LED pins and also doesnt keep sending junk to the SPI port

--- example/pico-fpga/firmware.c
+++ example/pico-fpga/firmware.c
@@ -19,9 +19,24 @@ int main() {
     ice_sdk_init();
     stdio_init_all();
 
-    ice_fpga_reset();
+    ice_fpga_init();
     ice_fpga_comm_init();
 
+    // Dont steal the RGB LED pins from the FPGA
+    gpio_set_dir(ICE_LED_RED_PIN, GPIO_IN);
+    gpio_set_dir(ICE_LED_GREEN_PIN, GPIO_IN);
+    gpio_set_dir(ICE_LED_BLUE_PIN, GPIO_IN);
+
+    ice_fpga_init_clock(12); // Setup the clock to the FPGA
+    ice_fpga_reset(); // Issue a good reset to the FPGA, check if it came up
+
+    while (1)
+    {
+        tud_task();
+    }
+    
+/*
+    // Example of writing/reading the FPGA SPI port:
     uint8_t write_data[DATA_LEN];
     uint8_t read_data[DATA_LEN];
     for (uint16_t i = 0; i < DATA_LEN; i++) {
@@ -33,4 +48,5 @@ int main() {
         ice_fpga_comm_write(START_ADDR, write_data, sizeof(write_data));
         ice_fpga_comm_read(read_data, START_ADDR, sizeof(read_data));
     }
+*/
 }
