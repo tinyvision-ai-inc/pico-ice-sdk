d1b81042d701d7f12a169b5f424ed4c72358833d
Josuah Demangeon <me@josuah.net>
2023-02-08 06:52:30 -0500

    remove ice_sdk.c and ice_sdk.h

--- src/ice_led.c
+++ src/ice_led.c
@@ -34,14 +34,15 @@ void ice_led_init(void)
     gpio_init(ICE_LED_GREEN_PIN);
     gpio_init(ICE_LED_BLUE_PIN);
 
+    // High-impedance by default, leaving LED control to the FPGA.
     gpio_set_dir(ICE_LED_RED_PIN, GPIO_IN);
     gpio_set_dir(ICE_LED_GREEN_PIN, GPIO_IN);
     gpio_set_dir(ICE_LED_BLUE_PIN, GPIO_IN);
 
     // A pull-down, which connects this end of the LED to the ground.
     gpio_put(ICE_LED_RED_PIN, false);
-    gpio_put(ICE_LED_GREEN_PIN, true);
-    gpio_put(ICE_LED_BLUE_PIN, true);
+    gpio_put(ICE_LED_GREEN_PIN, false);
+    gpio_put(ICE_LED_BLUE_PIN, false);
 }
 
 void ice_led_red(bool state)
