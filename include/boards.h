ab8f0683182faf452a78320f3ba251d39be33523
Josuah Demangeon <me@josuah.net>
2023-06-02 22:30:06 +0200

    remove out of date hardware version information

--- include/boards.h
+++ include/boards.h
@@ -6,5 +6,5 @@
 #define UF2_INDEX_URL       "https://pico-ice.readthedocs.io/"
 #define UF2_VOLUME_LABEL    "pico-ice"
 #define UF2_PRODUCT_NAME    "pico-ice"
-#define UF2_BOARD_ID        "pico-ice-v2.0"
+#define UF2_BOARD_ID        "pico-ice"
 #define BOARD_UF2_FAMILY_ID ICE_UF2_FAMILY_ID
