adc02b5e85db00d1c2ce95f8e9b389e2c3a8adbe
Mike Bell <mike@mercuna.com>
2023-09-01 22:00:59 +0100

    Improved combined programming of flash and SRAM

--- src/tinyuf2_board.c
+++ src/tinyuf2_board.c
@@ -16,47 +16,41 @@
 #include "ice_sram.h"
 
 static alarm_id_t alarm_id;
-static bool flash_ready;
-static bool sram_ready;
+static bool spi_ready;
 static uint32_t flash_erased[ICE_FLASH_SIZE_BYTES / (ICE_FLASH_BLOCK_SIZE * 32)];
 
 #define SRAM_ADDR 0x20000000
 
 void board_flash_read(uint32_t addr, void *buffer, uint32_t len)
 {
+    if (!spi_ready) {
+        ice_fpga_stop();
+        ice_sram_init();
+        ice_flash_init();
+        spi_ready = true;
+    }
     if (addr >= SRAM_ADDR) {
         addr -= SRAM_ADDR;
-        if (!sram_ready) {
-            ice_sram_init();
-            sram_ready = true;
-        }
         ice_sram_read_blocking(addr, buffer, len);
     }
     else {
-        if (!flash_ready) {
-            ice_flash_init();
-            flash_ready = true;
-        }
         ice_flash_read(addr, buffer, len);
     }
 }
 
 void board_flash_write(uint32_t addr, const void *data, uint32_t len)
 {
+    if (!spi_ready) {
+        ice_fpga_stop();
+        ice_sram_init();
+        ice_flash_init();
+        spi_ready = true;
+    }
     if (addr >= SRAM_ADDR) {
         addr -= SRAM_ADDR;
-        if (!sram_ready) {
-            ice_sram_init();
-            sram_ready = true;
-        }
         ice_sram_write_blocking(addr, data, len);
     }
     else {
-        if (!flash_ready) {
-            ice_fpga_stop();
-            ice_flash_init();
-            flash_ready = true;
-        }
         int flash_erase_idx = addr / ICE_FLASH_BLOCK_SIZE;
         if ((flash_erased[flash_erase_idx >> 5] & (1u << (flash_erase_idx & 0x1F))) == 0) {
             ice_flash_erase_block(addr & ~(ICE_FLASH_BLOCK_SIZE - 1));
