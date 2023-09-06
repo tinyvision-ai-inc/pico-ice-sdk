3a6d58d2bd94f0f66fcee5dc0757ce7e98c2e7b8
Josuah Demangeon <me@josuah.net>
2023-09-07 01:19:10 +0200

    examples: add a firmware/gateware combo

---
+++ examples/pico_ice/top.v
@@ -0,0 +1,17 @@
+/* Blink the RGB LED */
+
+module top (
+  input clock,
+  output LED_R,
+  output LED_G,
+  output LED_B
+);
+  reg [31:0] counter=0;
+
+  always@(posedge clock) begin
+    counter <= counter +1;
+  end
+
+  assign {LED_R,LED_G,LED_B} = counter[31:29];
+
+endmodule
