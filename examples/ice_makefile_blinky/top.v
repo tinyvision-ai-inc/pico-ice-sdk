5637e8d62fe6438826aa6de9913d904c2528cac4
Josuah Demangeon <me@josuah.net>
2023-09-07 12:01:22 +0200

    examples: add ice_makefile_blinky

---
+++ examples/ice_makefile_blinky/top.v
@@ -0,0 +1,21 @@
+
+module top (
+  input clk,
+  output led_blue,
+  output led_green,
+  output led_red
+);
+
+  localparam N = 24;
+
+  reg [N:0] counter;
+
+  always @(posedge clk) begin
+    counter <= counter + 1;
+  end
+
+  assign led_green = counter[N];
+  assign led_blue = counter[N - 1];
+  assign led_red = counter[N - 2];
+
+endmodule
