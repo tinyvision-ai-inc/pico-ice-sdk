bd18e4c9eab379f30c25dd9caa5f3991f39137da
Josuah Demangeon <me@josuah.net>
2023-09-07 15:23:31 +0200

    fix aio exmample

--- examples/ice_apio_blinky/top.v
+++ examples/ice_apio_blinky/top.v
@@ -1,21 +1,18 @@
-
 module top (
-  input clk,
-  output led_blue,
-  output led_green,
-  output led_red
+    input clk,
+    output led_blue,
+    output led_green,
+    output led_red
 );
+    localparam N = 24;
 
-  localparam N = 24;
+    reg [N-1:0] counter;
 
-  reg [N:0] counter;
+    always @(posedge clk) begin
+        counter <= counter + 1;
+    end
 
-  always @(posedge clk) begin
-    counter <= counter + 1;
-  end
-
-  assign led_green = counter[N];
-  assign led_blue = counter[N - 1];
-  assign led_red = counter[N - 2];
-
+    assign led_green = counter[N - 1];
+    assign led_blue = counter[N - 2];
+    assign led_red = counter[N - 3];
 endmodule
