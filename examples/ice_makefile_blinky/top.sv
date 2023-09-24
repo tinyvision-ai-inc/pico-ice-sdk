dfc0efeb73a3a8bf8cfa9fac3f135b1f0ccf5bcb
Josuah Demangeon <me@josuah.net>
2023-09-24 21:55:01 +0200

    examples: adjust the LED pinout

--- examples/ice_makefile_blinky/top.sv
+++ examples/ice_makefile_blinky/top.sv
@@ -1,12 +1,10 @@
 
 module top (
   input clk,
-  output led_blue,
   output led_green,
-  output led_red
 );
 
-  localparam N = 24;
+  localparam N = 22;
 
   reg [N:0] counter;
 
@@ -15,7 +13,5 @@ module top (
   end
 
   assign led_green = counter[N];
-  assign led_blue = counter[N - 1];
-  assign led_red = counter[N - 2];
 
 endmodule
