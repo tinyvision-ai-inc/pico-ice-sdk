4ebe348ffcb4feb3e2756d101f594679eb02d97c
Josuah Demangeon <me@josuah.net>
2024-06-20 23:12:41 +0200

    examples: ice_makefile_blinky: allow blinking with R, G or B easily

--- examples/ice_makefile_blinky/top.sv
+++ examples/ice_makefile_blinky/top.sv
@@ -1,17 +1,21 @@
 
 module top (
-  input clk,
-  output led_green,
+  input CLK,
+  output LED_R,
+  output LED_G,
+  output LED_B,
 );
 
   localparam N = 22;
 
   reg [N:0] counter;
 
-  always @(posedge clk) begin
+  always @(posedge CLK) begin
     counter <= counter + 1;
   end
 
-  assign led_green = counter[N];
+  assign LED_R = 1'b1;
+  assign LED_G = counter[N];
+  assign LED_B = 1'b1;
 
 endmodule
