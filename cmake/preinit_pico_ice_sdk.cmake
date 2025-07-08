fc7756ea3484653504084407bcc02f66cf52c924
Josuah Demangeon <me@josuah.net>
2025-07-08 21:47:23 +0000

    cmake: add a warning so that users of new board do not forget to adjust
    By default, the cmake helpers select the pico_ice board. This board
    is still widespread, but not manufactured anymore.
    
    New users will get started with the pico2-ice, try to find a compromise
    for current users and new users: default does not change, new users get
    a warning so they remember to set the -D flag.

--- cmake/preinit_pico_ice_sdk.cmake
+++ cmake/preinit_pico_ice_sdk.cmake
@@ -2,15 +2,18 @@ cmake_minimum_required(VERSION 3.13)
 
 add_library(pico_ice_preinit_common INTERFACE)
 
-if (NOT DEFINED PICO_BOARD OR PICO_BOARD STREQUAL pico_ice)
+if(NOT DEFINED PICO_BOARD OR PICO_BOARD STREQUAL pico_ice)
+    message(WARNING "if using the pico2-ice, add -DPICO_BOARD=pico2_ice at the end of the cmake command")
     set(PICO_BOARD pico_ice)
     target_compile_definitions(pico_ice_preinit_common INTERFACE PICO_ICE)
 endif()
-if (PICO_BOARD STREQUAL pico2_ice)
-    if (NOT DEFINED PICO_PLATFORM)
+
+if(PICO_BOARD STREQUAL pico2_ice)
+    if(NOT DEFINED PICO_PLATFORM)
         set(PICO_PLATFORM rp2350)
     endif()
     target_compile_definitions(pico_ice_preinit_common INTERFACE PICO2_ICE)
 endif()
+
 list(APPEND PICO_BOARD_HEADER_DIRS "${CMAKE_CURRENT_LIST_DIR}/../include/boards")
 
